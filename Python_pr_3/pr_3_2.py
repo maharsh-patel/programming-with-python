2a5f5c2cb479b2bf6ceebf27f79b38226407dc20
Maharsh_Patel13 <maharshpatel1306@gmail.com>
2026-01-22 23:54:08 +0530

    Update pr_3_2.py

--- Python_pr_3/pr_3_2.py
+++ Python_pr_3/pr_3_2.py
@@ -1,6 +1,14 @@
-i = 1
-n = int(input("enter a nuber:- "))
-while i <= 10:
-    multiplication = n*i
-    print(n, "X", i,"=", multiplication)
-    i= 1+i
+"""Write a program to check special number. (Number is equal to the sum of its divisors)157"""
+
+
+num = int(input("enter a number: "))
+
+sum = 0
+for i in range(1, num):
+    if num%i == 0:
+        sum = sum + i
+if sum==num:
+    print("The number", num, "is a special number")
+else:
+    print("The number", num, "is not a special number")
+
