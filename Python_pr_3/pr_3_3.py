1e5a4ebb40573752f3cb25933fbd814412ed580d
Maharsh_Patel13 <maharshpatel1306@gmail.com>
2026-01-22 22:31:31 +0530

    Update pr_3_3.py

--- Python_pr_3/pr_3_3.py
+++ Python_pr_3/pr_3_3.py
@@ -1,4 +1,8 @@
 #Create a program that will print out words that start with 's' from the below given statement.
 
 string = "sammys sells seashells by the seashore"
+
 print(string.split())
+for word in string.split():
+    if word.startswith('s'):
+        print(word)
